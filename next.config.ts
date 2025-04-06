5ba7511cec27df9170693cce59cfc845a8ee574b
dominusmars <khawammohamad99@gmail.com>
2025-04-05 21:04:30 -0400

    Update Next.js configuration to enable output export and react strict mode

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "/**",
-        search: "",
-      },
-    ],
-  },
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/**",
+                search: "",
+            },
+        ],
+    },
+    output: "export",
+    reactStrictMode: true,
 };
 
 export default nextConfig;
